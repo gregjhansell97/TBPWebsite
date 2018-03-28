ff9c1626c8b8dea908f370a387b33fce52ddfcb9
gregoryjhansell97 <gregjhansell@gmail.com>
2018-03-27 19:14:18 -0700

    adding an initial skeleton

---
+++ src/Root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Root extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+      </div>
+    );
+  }
+}
+
+export default Root;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Root from "./Root.jsx"s
+
+ReactDOM.render(<Root/>, document.getElementById('app'));
